43d21970660cb5a4fa95fae6f4f8615242e506ce
Kalon <kalon.cheong@gmail.com>
2014-11-11 16:55:22 -0500

    clean up. and adding new design.
    separating algorithms into their own class.

--- src/com/lonkal/convexhullapp/main/ComputeCHButton.java
+++ src/com/lonkal/convexhullapp/main/ComputeCHButton.java
@@ -5,10 +5,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.LinkedList;
 
+import javax.swing.Timer;
+
 public class ComputeCHButton implements ActionListener {
 
 	private GraphPanel graphPanel;
-
+	private ConvexHullAlgo currentAlgo;
+	
 	public ComputeCHButton(GraphPanel gp) {
 		graphPanel = gp;
 	}
@@ -24,14 +27,17 @@ public class ComputeCHButton implements ActionListener {
 	}
 
 	public void jarvisMarch() {
+		Timer t = graphPanel.getTaskTimer();
+
 		LinkedList<Point> convexHullList = new LinkedList<Point>();		
 		LinkedList<Point> pointList = graphPanel.getPointList();
 		
 		graphPanel.addCHList(convexHullList);
-		//convexHullList.add(getLeftmostPoint(pointList));
 		
 		Point hullPoint = getLeftmostPoint(pointList);
 		Point endPoint;
+		
+		t.start();
 		do{
 			endPoint = pointList.getFirst();
 			convexHullList.add(hullPoint);
@@ -39,7 +45,12 @@ public class ComputeCHButton implements ActionListener {
 			//Walk through the pointList
 			for (Point p : pointList) {
 				System.out.println(p);
-				//graphPanel.updateStep(hullPoint, p);
+				//Send a draw event
+				//Wait for draw event + pause
+				//Comeback and keep going
+				System.out.println(Thread.currentThread().getName());
+				graphPanel.updateStep(hullPoint, p);
+				
 				//System.out.println(Primitives.orientation(hullPoint, endPoint, p));
 				if (endPoint == hullPoint || Primitives.orientation(hullPoint, endPoint, p) == 2) {
 					endPoint = p;
@@ -47,11 +58,12 @@ public class ComputeCHButton implements ActionListener {
 			}
 			hullPoint = endPoint;
 
-			graphPanel.repaint();
+			//graphPanel.repaint();
 			System.out.println(convexHullList);
 
 		} while (endPoint != convexHullList.getFirst());
 		convexHullList.add(convexHullList.getFirst());
+		t.stop();
 		System.out.println(convexHullList);
 	}
 
