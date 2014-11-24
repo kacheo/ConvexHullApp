1d2d767eb1249af1da7b9b6b4f7d22475a3c3e31
Kalon <kalon.cheong@gmail.com>
2014-11-24 13:57:21 -0500

    release v1.1

--- src/com/lonkal/convexhullapp/main/CHAppSettings.java
+++ src/com/lonkal/convexhullapp/main/CHAppSettings.java
@@ -16,5 +16,5 @@ public final class CHAppSettings {
 	public static final String[] CH_ALGORITHMS_LIST = { CH_JARVIS_MARCH_NAME,
 			CH_MONOTONE_CHAIN_NAME, CH_SWEEPHULL_NAME };
 
-	public static final String ABOUT_APP_MESSAGE = "Convex Hull App | v 1.0\nKalon Cheong (c) 2014\n\nServing all your Convex Hull Algorithm simulation needs :)";
+	public static final String ABOUT_APP_MESSAGE = "Convex Hull App | v 1.1\nKalon Cheong (c) 2014\n\nServing all your Convex Hull Algorithm simulation needs :)";
 }
