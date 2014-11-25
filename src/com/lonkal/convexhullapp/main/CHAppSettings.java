9d9aec329b09c3daf10afedb1bca754bafb348d9
Kalon <kalon.cheong@gmail.com>
2014-11-25 12:43:59 -0500

    clean up settings class

--- src/com/lonkal/convexhullapp/main/CHAppSettings.java
+++ src/com/lonkal/convexhullapp/main/CHAppSettings.java
@@ -1,12 +1,14 @@
 package com.lonkal.convexhullapp.main;
 
 /**
- * This class holds any static strings related to the application.
+ * This class holds any static variables related to the application.
  * 
  * @author Kal
  *
  */
 public final class CHAppSettings {
+
+	public static final String ABOUT_APP_MESSAGE = "Convex Hull App | v 1.1\nKalon Cheong (c) 2014\n\nServing all your Convex Hull Algorithm simulation needs :)";
 
 	public static final String CH_JARVIS_MARCH_NAME = "Jarvis March";
 	public static final String CH_MONOTONE_CHAIN_NAME = "Monotone Chain Algorithm";
@@ -16,5 +18,4 @@ public final class CHAppSettings {
 	public static final String[] CH_ALGORITHMS_LIST = { CH_JARVIS_MARCH_NAME,
 			CH_MONOTONE_CHAIN_NAME, CH_SWEEPHULL_NAME };
 
-	public static final String ABOUT_APP_MESSAGE = "Convex Hull App | v 1.1\nKalon Cheong (c) 2014\n\nServing all your Convex Hull Algorithm simulation needs :)";
 }
