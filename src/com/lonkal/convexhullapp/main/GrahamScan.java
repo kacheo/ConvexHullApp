3247632fbe7ec2b826d89de5dae0475cbcf910fd
Kalon <kalon.cheong@gmail.com>
2014-11-26 00:07:52 -0500

    working graham scan

--- src/com/lonkal/convexhullapp/main/GrahamScan.java
+++ src/com/lonkal/convexhullapp/main/GrahamScan.java
@@ -10,8 +10,17 @@ public class GrahamScan extends ConvexHullAlgo {
 
 	private LinkedList<Point> stepPointList;
 
+	private int i = 2; // starts at the "3rd" array element
+
+	private boolean isDone;
+	private Point lowestRightestPt;
+
+	private Point p, q, r;
+
 	public GrahamScan(LinkedList<Point> list) {
 		super(list);
+		stepPointList = new LinkedList<Point>();
+
 		init();
 	}
 
@@ -25,6 +34,13 @@ public class GrahamScan extends ConvexHullAlgo {
 
 		@Override
 		public int compare(Point p1, Point p2) {
+
+			// comparisonPoint is default bigger then anything, to be at the end
+			if (p1 == comparisonPoint) {
+				return 1;
+			} else if (p2 == comparisonPoint){
+				return -1;
+			}
 
 			// Basically, the angle of the points are calculated from the the
 			// comparisonPoint
@@ -65,7 +81,7 @@ public class GrahamScan extends ConvexHullAlgo {
 
 		// Find lowest and bottommost point
 		Iterator<Point> it = pointList.iterator();
-		Point lowestRightestPt = pointList.getFirst();
+		lowestRightestPt = pointList.getFirst();
 		while (it.hasNext()) {
 			Point p = it.next();
 			if (p.y > lowestRightestPt.y) {
@@ -75,21 +91,45 @@ public class GrahamScan extends ConvexHullAlgo {
 						: p;
 			}
 		}
-		pointList.remove(lowestRightestPt);
 
 		// Sort all points based on angle
 		Collections.sort(pointList, new AngleComparator(lowestRightestPt));
-		stepPointList = pointList;
+
+		convexHullList.add(lowestRightestPt);
+		convexHullList.add(pointList.getFirst());
+		convexHullList.add(pointList.get(1));
 	}
 
 	@Override
 	public void step() {
+		if (isDone) {
+			return;
+		}
 
+		stepNum++;
+
+		if (i >= pointList.size()) {
+			isDone = true;
+			convexHullList.add(lowestRightestPt);
+			pointList.add(lowestRightestPt); // Add it back so it is rendered
+			return;
+		}
+
+		p = convexHullList.get(convexHullList.size() - 2);
+		q = convexHullList.get(convexHullList.size() - 1);
+		r = pointList.get(i);
+
+		if (Primitives.orientation(p, q, r) <= 1 || i == pointList.size() - 1) {
+			convexHullList.add(r); // We're good
+			i++;
+		} else if (Primitives.orientation(p, q, r) == 2) {
+			convexHullList.remove(q);
+		}
 	}
 
 	@Override
 	public boolean isDone() {
-		return false;
+		return isDone;
 	}
 
 	@Override
@@ -99,6 +139,14 @@ public class GrahamScan extends ConvexHullAlgo {
 
 	@Override
 	public LinkedList<Point> getCurrentStepPoints() {
+		if (p == null || q == null || r == null) {
+			return null;
+		}
+
+		stepPointList.clear();
+		stepPointList.add(p);
+		stepPointList.add(q);
+		stepPointList.add(r);
 		return stepPointList;
 	}
 
